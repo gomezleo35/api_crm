75ed3efb0402182a9fde834be1737117f943ea88
Leonardo Gomez <gomezleo35@gmail.com>
2022-07-25 22:41:23 -0300

    first commit

---
+++ src/paginas/EditarCliente.jsx
@@ -0,0 +1,42 @@
+import Formulario from "../Components/Formulario"
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+
+const EditarCliente = () => {
+
+  const {id} = useParams()
+    const [cliente, setCliente] = useState({})
+    const [cargando, setCargando] = useState(true)
+    
+    useEffect(() => {
+        const obtenerClientesAPI = async () => {
+            try {
+                const url = `http://localhost:4000/clientes/${id}`
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+                setCliente(resultado)
+            } catch (error) {
+                console.log(error)
+            }
+            setTimeout(() => {
+                setCargando(false)
+            }, 3000);
+        }
+        obtenerClientesAPI()
+    }, [])
+  
+  return (
+    <>
+        <h1 className='font-black text-4xl text-blue-900'>Editar cliente</h1>
+        <p className='mt-3 font-bold text-2xl'>Utilizar este formulario para editar los datos de un cliente</p>
+        {cliente?.nombre ? (
+          <Formulario 
+          cliente={cliente}
+          cargando={cargando}
+        />
+        ) : <p>Cliente ID no valido</p>}
+    </>
+  )
+}
+
+export default EditarCliente
