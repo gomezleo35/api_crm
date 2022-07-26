b9021e42608b53313f5a966d32fc85ab9aabcaff
Leonardo Gomez <gomezleo35@gmail.com>
2022-07-25 23:45:02 -0300

    se agregan variables de entorno

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -32,7 +32,7 @@ const navigate = useNavigate()
         try{
             let respuesta
             if (cliente.id) {
-                const url = `http://localhost:4000/clientes/${cliente.id}`
+                const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
                 respuesta = await fetch(url, {
                 method: 'PUT',
                 body: JSON.stringify(valores),
@@ -41,7 +41,7 @@ const navigate = useNavigate()
                 }
                 })
             } else {
-                const url = 'http://localhost:4000/clientes'
+                const url = import.meta.env.VITE_API_URL
                 respuesta = await fetch(url, {
                 method: 'POST',
                 body: JSON.stringify(valores),
